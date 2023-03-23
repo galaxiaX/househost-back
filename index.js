6b37dc12b7422c22e23e75877f0d048afc0f6b93
Namo <pongsathon139@gmail.com>
2023-03-23 11:40:22 +0700

    try to fix cors 2

--- index.js
+++ index.js
@@ -28,21 +28,21 @@ const generateFileName = (bytes = 32) =>
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.MAIN_URL || "http://localhost:5173",
-  })
-);
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    process.env.MAIN_URL || "http://localhost:5173"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use(
+//   cors({
+//     origin: process.env.MAIN_URL || "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+// app.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     process.env.MAIN_URL || "http://localhost:5173"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 mongoose.connect(process.env.MONGO_URL);
 
