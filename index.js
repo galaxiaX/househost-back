6cf55baa8dbbfb60d4c540d9735924ded2f2adf8
Namo <pongsathon139@gmail.com>
2023-03-23 11:44:24 +0700

    try to fix cors 3

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ const generateFileName = (bytes = 32) =>
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: process.env.MAIN_URL || "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.MAIN_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 // app.use((req, res, next) => {
 //   res.header(
 //     "Access-Control-Allow-Origin",
