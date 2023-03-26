d4bfbcf841f7bd0821bdfc2c4a7f0d84641185fc
Namo <pongsathon139@gmail.com>
2023-03-26 12:12:24 +0700

    try to fix logout 10

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.get("/profile", async (req, res) => {
 
 app.post("/logout", (req, res) => {
   res
-    .cookie("token", null, {
+    .cookie("token", "", {
       // domain: "househost-back.vercel.app",
       sameSite: "none",
       secure: true,
